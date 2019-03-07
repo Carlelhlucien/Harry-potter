d379c011a38cd32bf8553ecbaac29ed9c3839de3
Carlelhlucien <43241774+Carlelhlucien@users.noreply.github.com>
2019-03-07 09:35:31 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".trick1st").click(function() {
+$(".trickOne").fadeToggle();
+});
+$(".trick2nd").click(function() {
+$(".trickTwo").slideToggle();
+});
+$(".trick3rd").click(function() {
+$(".trickthree").toggle();
+});
